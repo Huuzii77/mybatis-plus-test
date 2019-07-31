31212b2bc3950a221846647297929922b7333308
chendesheng <chendesheng@tuhu.cn>
2019-07-31 22:16:19 +0800

    mybatis-plus搭建

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @author chendesheng chendesheng@tuhu.cn
+ * @since 2019/7/31 14:16
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.User;
+
+/**
+ * @author chendesheng chendesheng@tuhu.cn
+ * @since 2019/7/31 14:19
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
